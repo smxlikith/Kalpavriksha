fbeceb769dd52546b9877777c16e3086b3b37a2f
Likith Mohan <131691876+smxlikith@users.noreply.github.com>
2025-10-17 13:22:36 +0530

    Added a Non-recursive Solution

--- Assignment-4/sonarImageProcessing.c
+++ Assignment-4/sonarImageProcessing.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #define MAX_VAL 255
 
+void display(int size, int** matrix);
+
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
@@ -61,18 +63,35 @@ int smoothing3x3(int i, int j, int size, int** matrix) {
     return currentSum / validNeighbors;
 }
 
-void applySmoothing(int i, int j, int size, int** matrix) {
+void applySmoothingRecursive(int i, int j, int size, int** matrix) {
     if (i >= size || i < 0) {
         return;
     }
     if (j >= size || j < 0) {
-        applySmoothing(i + 1, 0, size, matrix);
+        applySmoothingRecursive(i + 1, 0, size, matrix);
         return;
     }
 
     int currentSmoothedValue = smoothing3x3(i, j, size, matrix);
-    applySmoothing(i, j + 1, size, matrix);
+    applySmoothingRecursive(i, j + 1, size, matrix);
     matrix[i][j] = currentSmoothedValue;
+}
+
+void applySmoothingNonRecursive(int size, int** matrix) {
+    int* curr = calloc(size, sizeof(int));
+    int* prev = calloc(size, sizeof(int));
+
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j < size; j++) {
+            prev[j] = curr[j];
+            curr[j] = smoothing3x3(i, j, size, matrix);
+        }
+        if (i >= 1) {
+            for (int j = 0; j < size; j++) {
+                matrix[i - 1][j] = prev[j];
+            }
+        }
+    }
 }
 
 void display(int size, int** matrix) {
@@ -107,9 +126,13 @@ int main() {
     display(size, matrix);
     printf("\n");
 
-    printf("smoothing the matrix\n");
-    applySmoothing(0, 0, size, matrix);
+    printf("smoothing the matrix:\n");
+    applySmoothingRecursive(0, 0, size, matrix);
     display(size, matrix);
+
+    // printf("smoothing the matrix Non-recursive:\n");
+    // applySmoothingNonRecursive(size, matrix);
+    // display(size, duplicate);
 
     cleanUp(size, matrix);
     return 0;
