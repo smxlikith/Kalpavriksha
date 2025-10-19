6800dfff4d807997fd0279b6a28ca901301a3418
Likith Mohan <131691876+smxlikith@users.noreply.github.com>
2025-10-19 14:31:48 +0530

    Update the Code: - Fixed the Matrix references to use pointer arithmetics. - Changed the name of the function display to displayMatrix. - Changed the name of the function smoothing3x3 to calculate3x3NeighborsAverage.

--- Assignment-4/sonarImageProcessing.c
+++ Assignment-4/sonarImageProcessing.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #define MAX_VAL 255
 
-void display(int size, int** matrix);
-
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
@@ -19,18 +17,18 @@ int** generateMatrix(int size) {
     }
 
     for (int i = 0; i < size; i++) {
-        matrix[i] = calloc(size, sizeof(int));
+        *(matrix + i) = calloc(size, sizeof(int));
 
-        if (matrix[i] == NULL) {
+        if (matrix + i == NULL) {
             for (int k = 0; k > i; k++) {
-                free(matrix[k]);
+                free(matrix + k);
             }
             printf("Memory allocation failed\n");
             exit(0);
         }
 
         for (int j = 0; j < size; j++) {
-            matrix[i][j] = rand() % (MAX_VAL + 1);
+            *(*(matrix + i) + j) = rand() % (MAX_VAL + 1);
         }
     }
     return matrix;
@@ -39,23 +37,23 @@ int** generateMatrix(int size) {
 void rotateMatrix(int size, int** matrix) {
     for (int i = 0; i < size; i++) {
         for (int j = i; j < size; j++) {
-            swap(&matrix[i][j], &matrix[j][i]);
+            swap(*(matrix + i) + j, *(matrix + j) + i);
         }
     }
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size / 2; j++) {
-            swap(&matrix[i][j], &matrix[i][size - j - 1]);
+            swap(*(matrix + i) + j, *(matrix + i) + (size - j - 1));
         }
     }
 }
 
-int smoothing3x3(int i, int j, int size, int** matrix) {
+int calculate3x3NeighborsAverage(int i, int j, int size, int** matrix) {
     int validNeighbors = 0;
     int currentSum = 0;
     for (int x = i - 1; x <= i + 1; x++) {
         for (int y = j - 1; y <= j + 1; y++) {
             if (x >= 0 && y >= 0 && x < size && y < size) {
-                currentSum += matrix[x][y];
+                currentSum += *(*(matrix + x) + y);
                 validNeighbors++;
             }
         }
@@ -72,9 +70,9 @@ void applySmoothingRecursive(int i, int j, int size, int** matrix) {
         return;
     }
 
-    int currentSmoothedValue = smoothing3x3(i, j, size, matrix);
+    int currentSmoothedValue = calculate3x3NeighborsAverage(i, j, size, matrix);
     applySmoothingRecursive(i, j + 1, size, matrix);
-    matrix[i][j] = currentSmoothedValue;
+    *(*(matrix + i) + j) = currentSmoothedValue;
 }
 
 void applySmoothingNonRecursive(int size, int** matrix) {
@@ -83,21 +81,21 @@ void applySmoothingNonRecursive(int size, int** matrix) {
 
     for (int i = 0; i <= size; i++) {
         for (int j = 0; j < size; j++) {
-            prev[j] = curr[j];
-            curr[j] = smoothing3x3(i, j, size, matrix);
+            *(prev + j) = *(curr + j);
+            *(curr + j) = calculate3x3NeighborsAverage(i, j, size, matrix);
         }
         if (i >= 1) {
             for (int j = 0; j < size; j++) {
-                matrix[i - 1][j] = prev[j];
+                *(*(matrix + (i - 1)) + j) = *(prev + j);
             }
         }
     }
 }
 
-void display(int size, int** matrix) {
+void displayMatrix(int size, int** matrix) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            printf("%d, ", matrix[i][j]);
+            printf("%d, ", *(*(matrix + i) + j));
         }
         printf("\n");
     }
@@ -105,7 +103,7 @@ void display(int size, int** matrix) {
 
 void cleanUp(int size, int** matrix) {
     for (int i = 0; i < size; i++) {
-        free(matrix[i]);
+        free(*(matrix + i));
     }
     free(matrix);
 }
@@ -118,21 +116,21 @@ int main() {
     int** matrix = generateMatrix(size);
 
     printf("Generated the matrix\n");
-    display(size, matrix);
+    displayMatrix(size, matrix);
     printf("\n");
 
     printf("rotating the matrix\n");
     rotateMatrix(size, matrix);
-    display(size, matrix);
+    displayMatrix(size, matrix);
     printf("\n");
 
     printf("smoothing the matrix:\n");
     applySmoothingRecursive(0, 0, size, matrix);
-    display(size, matrix);
+    displayMatrix(size, matrix);
 
     // printf("smoothing the matrix Non-recursive:\n");
     // applySmoothingNonRecursive(size, matrix);
-    // display(size, duplicate);
+    // displayMatrix(size, matrix);
 
     cleanUp(size, matrix);
     return 0;
